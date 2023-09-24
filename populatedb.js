2dea470c2ea55b91563407fd6ac4dd6af768ffc6
Sarah Quinney <quinney.sarah@gmail.com>
2023-09-24 15:59:38 -0500

    database populated, id set up for user messages

--- populatedb.js
+++ populatedb.js
@@ -23,7 +23,7 @@ async function main() {
     await getMessages();
     console.log("Debug: Closing mongoose");
     mongoose.connection.close();
-  }
+}
 
 //create functions to create the user and message objects
 async function userCreate(first_name, last_name, username, password, member, admin) {
@@ -39,6 +39,7 @@ async function userCreate(first_name, last_name, username, password, member, adm
     const user = new User(userDetail);
 
     await user.save();
+    // users[index]= user
     console.log(`Added User: ${user}`);
 }
 
@@ -55,62 +56,90 @@ async function messageCreate(title, timestamp, message_text) {
     console.log(`Added Message: ${message}`)
 }
 
-//add some sample users and messages to the database
 async function getUsers() {
     console.log("Adding Users")
     await Promise.all([
-        userCreate(
-            "Sam",
-            "Jones",
-            "sam_jones34",
-            "abewfo31!?",
-            false,
-            false,
-        ),
-        userCreate(
-            "Ruth",
-            "Smith",
-            "ruthisgreat233",
-            "ceoenvoevnowihc",
-            true,
-            false,
-        ),
-        userCreate(
-            "Jarod",
-            "Light",
-            "lights784sky",
-            "vevbobevoc003",
-            true,
-            true,
-        ),
-        userCreate(
-            "Test",
-            "Account",
-            "test2023",
-            "account2023",
-            true,
-            true,
-        )
+        userCreate()
     ])
 }
 
 async function getMessages() {
     console.log("Adding Messages")
     await Promise.all([
-        messageCreate(
-            "Hello There",
-            Date.now(),
-            "How is everyone doing today? I am doing well."
-        ),
-        messageCreate(
-            "Hi All",
-            Date.now(),
-            "What is the weather like today in your town? Mine is clowdy!"
-        ),
-        messageCreate(
-            "Anyone Out There",
-            Date.now(),
-            "I just dropped in to say hello!"
-        )
+        messageCreate()
     ])
 }
+// //add some sample users and messages to the database
+// async function getUsers() {
+//     console.log("Adding Users")
+//     await Promise.all([
+//         userCreate(
+//             "Sam",
+//             "Jones",
+//             "sam_jones34",
+//             "abewfo31!?",
+//             true,
+//             false,
+//         ),
+//         userCreate(
+//             "Ruth",
+//             "Smith",
+//             "ruthisgreat233",
+//             "ceoenvoevnowihc",
+//             true,
+//             false,
+//         ),
+//         userCreate(
+//             "Jarod",
+//             "Light",
+//             "lights784sky",
+//             "vevbobevoc003",
+//             true,
+//             true,
+//         ),
+//         userCreate(
+//             "Test",
+//             "Account",
+//             "test2023",
+//             "account2023",
+//             true,
+//             true,
+//         )
+//     ])
+// }
+
+// async function getMessages() {
+//     console.log("Adding Messages")
+//     await Promise.all([
+//         messageCreate(
+//             users[0]._id,
+//             "Hello There",
+//             Date.now(),
+//             "How is everyone doing today? I am doing well."
+//         ),
+//         messageCreate(
+//             users[0]._id,
+//             "Hi All",
+//             Date.now(),
+//             "What is the weather like today in your town? Mine is clowdy!"
+//         ),
+//         messageCreate(
+//             users[1]._id,
+//             "Anyone Out There",
+//             Date.now(),
+//             "I just dropped in to say hello!"
+//         ),
+//         messageCreate(
+//             users[2]._id,
+//             "Today is Sunday.",
+//             Date.now(),
+//             "Yay, Sunday!"
+//         ),
+//         messageCreate(
+//             users[3]._id,
+//             "Tomorrow is Monday.",
+//             Date.now(),
+//             " Booo Monday!"
+//         )
+//     ])
+// }
