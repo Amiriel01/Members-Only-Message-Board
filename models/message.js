c37178aceaf1c12367685a4bf9d01d7b5fbe3313
Sarah Quinney <quinney.sarah@gmail.com>
2023-09-24 12:22:34 -0500

    created user and message schema in models folder

---
+++ models/message.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 25,
+    },
+    timestamp: { 
+        type: Date, default: Date.now, required: true 
+    },
+    message_text: {
+        type: String,
+        required: true,
+        minLength: 1,
+        maxLength: 250,
+    }
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
+
+// { timestamps: true },
+    // timestamps: {
+    //     createdAt: 'created_at',
+    //     updatedAt: 'updated_at',
+    // }
---
+++ models/user.js
@@ -0,0 +1,39 @@
+// Require Mongoose
+const mongoose = require("mongoose");
+
+// Define a schema
+const Schema = mongoose.Schema;
+
+//create user schema
+const UserSchema = new Schema({
+    first_name: { 
+        type: String, required: true, minLength: 1, maxLength: 25 
+    },
+    last_name: { 
+        type: String, required: true, minLength: 1, maxLength: 25 
+    },
+    username: {
+        type: String, 
+        required: true,
+        unique: true,
+        minLength: 5,
+        maxLength: 25,
+    },
+    password: {
+        type: String, 
+        required: true,
+        unique: true,
+        maxLength: 25,
+    },
+    member: {
+        type: Boolean,
+        default: false,
+    },
+    admin: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+module.exports = mongoose.model("User", UserSchema);
+
