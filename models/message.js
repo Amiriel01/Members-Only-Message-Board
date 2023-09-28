8e40f20137e660f78a1311adf9fd13734dde07b1
Sarah Quinney <quinney.sarah@gmail.com>
2023-09-28 17:40:38 -0500

    non-user message list page formatted and styled

--- models/message.js
+++ models/message.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
-
+const { DateTime } = require("luxon");
 const Schema = mongoose.Schema;
+
 
 const MessageSchema = new Schema({
     user: {
@@ -25,6 +26,10 @@ const MessageSchema = new Schema({
 
 MessageSchema.virtual("url").get(function() {
     return `/routers/message/${this._id}`;
+})
+
+MessageSchema.virtual("timestamp_formatted").get(function () {
+    return DateTime.fromJSDate(this.timestamp).toLocaleString(DateTime.DATETIME_SHORT);
 })
 
 module.exports = mongoose.model("Message", MessageSchema);
